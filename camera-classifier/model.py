e5a3a6bfb0330003107708f41d8d9ac0d5e9a0c4
unknown <sddoshigcp@gmail.com>
2024-12-25 10:47:23 -0600

    Added comments

--- camera-classifier/model.py
+++ camera-classifier/model.py
@@ -10,16 +10,19 @@ class Model:
         # using LinearSVC, in production-level project should use neural network
         self.model = LinearSVC()
 
+    # train model
     def train_model(self, counters):
         img_list = np.array([])
         class_list = np.array([])
 
+        # first class
         for i in range(1, counters[0]):
             img = cv.imread(f'1/frame{i}.jpg')[:, :, 0]
             img = img.reshape(16950)
             img_list = np.append(img_list, [img])
             class_list = np.append(class_list, 1)
 
+        # second class
         for i in range(1, counters[1]):
             img = cv.imread(f'2/frame{i}.jpg')[:, :, 0]
             img = img.reshape(16950)
@@ -27,6 +30,8 @@ class Model:
             class_list = np.append(class_list, 2)
 
         img_list = img_list.reshape(counters[0] - 1 + counters[1] - 1, 16950)
+
+        # use linear svc to fit image and class list to model
         self.model.fit(img_list, class_list)
         print("Model successfully trained.")
 
@@ -39,6 +44,8 @@ class Model:
 
         img = cv.imread('frame.jpg')[:, :, 0]
         img = img.reshape(16950)
+
+        # predict class of image from current frame  using model
         prediction = self.model.predict([img])
 
         return prediction[0]
